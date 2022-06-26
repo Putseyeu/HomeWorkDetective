698a4269123d934c0ae14eb7f6cd84e3af1e14fc
Putseyeu <putseyeu.a.d@gmail.com>
2022-06-26 23:03:42 +0200

    step1

---
+++ Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkDetective
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase dataBase = new DataBase();
+            bool isWork = true;
+            Console.WriteLine("Программа для поиска преступников.");
+
+            while (isWork == true)
+            {
+                Console.WriteLine(" 1 - поиск преступника по заданным параметрам. 2 - выход из программы.");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        dataBase.SearchPeople();
+                        break;
+
+                    case "2":
+                        isWork = false;
+                        break;
+                }
+            }
+        }
+    }
+
+    class DataBase
+    {
+        private List<Criminal> _criminals = new List<Criminal>();
+
+        public DataBase()
+        {
+            CreateList();
+        }
+
+        public void SearchPeople()
+        {
+            int growth;
+            int weight;
+            string nationality;
+            Console.WriteLine("Поиск человека");
+            Console.Write("Укажите рост: ");
+            growth = GetInputNumber();
+
+            if (growth != 0)
+            {
+                Console.Write("Укажите вес: ");
+                weight = GetInputNumber();
+
+                if (weight != 0)
+                {
+                    Console.Write("Укажите национальность: ");
+                    nationality = GetInputText();
+
+                    if (nationality != null)
+                    {
+                        var filteredCriminal = from Criminal criminal in _criminals
+                                               where criminal.Growth == growth
+                                               || criminal.Weight == weight
+                                               || criminal.Nationality.ToUpper() == nationality.ToUpper()
+                                               select criminal;
+
+                        foreach (var criminal in filteredCriminal)
+                        {
+                            if(criminal.InCustody == false)
+                            {
+                                criminal.ShowInfo();
+                            }                           
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введены не коректные данные");
+            }           
+        }
+
+        private int GetInputNumber()
+        {
+            int maxLength = 3;
+            bool isNumber = int.TryParse(Console.ReadLine(), out int inputNumber);
+
+            if (isNumber == true && inputNumber >= maxLength)
+            {
+                return inputNumber;
+            }
+            else
+            {
+                inputNumber = 0;
+                return inputNumber;
+            }
+        }
+
+        private string GetInputText()
+        {
+            string text = Console.ReadLine();
+
+            foreach (char symbol in text)
+            {
+                if (char.IsLetter(symbol) == false)
+                {
+                    return null;
+                }
+            }
+
+            return text;
+        }
+
+        private void CreateList()
+        {
+            _criminals.Add(new Criminal("Евгений", "Рыкавец", "Анатольевичь", 185, 90, "китаец", false));
+            _criminals.Add(new Criminal("Кирилл", "Фидотов", "Андреевичь", 160, 100, "мангол", true));
+            _criminals.Add(new Criminal("Игорь", "Зйцев", "Петровичь", 200, 120, "мараканец", false));
+            _criminals.Add(new Criminal("Евгений", "Мухавец", "Дмитриевичь", 170, 90, "китаец", true));
+        }
+    }
+
+    class Criminal
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Patronymic { get; private set; }
+        public bool InCustody { get; private set; }
+        public int Growth { get; private set; }
+        public int Weight { get; private set; }
+        public string Nationality { get; private set; }
+
+        public Criminal(string name, string surname, string patronymic, int growth, int weight, string nationality, bool inCustody)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            Growth = growth;
+            Weight = weight;
+            Nationality = nationality;
+            InCustody = inCustody;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"ФИО : {Name} {Surname} {Patronymic} | Рост {Growth} | Вес {Weight} | Национальность {Nationality}|");
+        }
+    }
+}
